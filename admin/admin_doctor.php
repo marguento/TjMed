4dbee9a21b71d60ebb8a0b9b71cd4bf662532371
eirarangel <eira.rangel@gmail.com>
2014-08-26 22:53:28 -0700

    Cambios en admin
    Agregue usuarios y modifique doctores

--- admin/admin_doctor.php
+++ admin/admin_doctor.php
@@ -1,4 +1,13 @@
 <h2 class="sub-header">Administración doctores</h2>
+
+<div class="tabbable">
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#tab1-1" data-toggle="tab">Lista</a></li>
+    <li class=""><a href="#tab1-2" data-toggle="tab">Verificar <span class="badge">4</span></a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="tab1-1">
+
           <center> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Agregar doctor</button> </center>
           <div class="space20"></div>
           <div class="row">
@@ -12,6 +21,7 @@
             <table class="table table-striped">
               <thead>
                 <tr>
+                  <th id="user_head"></th>
                   <th>Nombre</th>
                   <th>Categoría</th>
                   <th>Seleccionar</th>
@@ -19,22 +29,26 @@
               </thead>
               <tbody>
                 <tr>
-                  <td><a href="../index.php?opcion=perfil" target="_blank">Dra. Eira Rangel</a></td>
+                  <td><img class="user_img" src="../images/melissa.jpg"></td>
+                  <td><a href="../admin/index.php?opcion=doctor_profile" target="_blank">Dra. Eira Rangel</a></td>
                   <td>Bariatría</td>
                   <td><input type="checkbox"></td>
                 </tr>
                 <tr>
-                  <td><a href="../index.php?opcion=perfil" target="_blank">Dr. Orlando Macías</a></td>
+                  <td><img class="user_img" src="../images/macias.jpeg"></td>
+                  <td><a href="../admin/index.php?opcion=doctor_profile" target="_blank">Dr. Orlando Macías</a></td>
                   <td>Dentista</td>
                   <td><input type="checkbox"></td>
                 </tr>
                 <tr>
-                  <td><a href="../index.php?opcion=perfil" target="_blank">Dr. Hamlet</a></td>
+                  <td><img class="user_img" src="../images/hector.jpg"></td>
+                  <td><a href="../admin/index.php?opcion=doctor_profile" target="_blank">Dr. Hamlet</a></td>
                   <td>Pediatría</td>
                   <td><input type="checkbox"></td>
                 </tr>
                 <tr>
-                  <td><a href="../index.php?opcion=perfil" target="_blank">Dr. Max Valenzuela</a></td>
+                  <td><img class="user_img" src="../images/max.jpg"></td>
+                  <td><a href="../admin/index.php?opcion=doctor_profile" target="_blank">Dr. Max Valenzuela</a></td>
                   <td>Ginecología</td>
                   <td><input type="checkbox"></td>
                 </tr>
@@ -58,7 +72,80 @@
             
           </div>
         </div>
+        <div class="tab-pane" id="tab1-2">
+
+          <div class="space20"></div>
+          <div class="row">
+            <div class="col-sm-9"></div>
+            <div class="col-sm-3">
+           <input type="text" class="form-control" placeholder="Search">
+           </div>
+          </div>
+           <div class="space20"></div>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th> Nombre</th>
+                  <th> Creado por </th>
+                  <th> Fecha de registro </th>
+                  <th> Categoría</th>
+                  <th> Seleccionar</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><a href="#" data-toggle="modal" data-target="#verified_doc">Dra. Virginia Woolf <span class="label label-success">Nuevo</span></a></td>
+                  <td><a href="#">Jorge Ramírez</a></td>
+                  <td>27 Agosto 2014</td>
+                  <td>Bariatría</td>
+                  <td><input type="checkbox"></td>
+                </tr>
+                <tr>
+                  <td><a href="#" data-toggle="modal"  data-target="#verified_doc">Dr. Franz Kafka</a></td>
+                  <td><a href="#">Jorge Ramírez</a></td>
+                  <td>20 Agosto 2014</td>
+                  <td>Dentista</td>
+                  <td><input type="checkbox"></td>
+                </tr>
+                <tr>
+                  <td><a href="#" data-toggle="modal"  data-target="#verified_doc">Dr. Frankestein <span class="label label-success">Nuevo</span></a></td>
+                  <td><a href="#">Pepe Le Pew</a></td>
+                  <td>27 Agosto 2014</td>
+                  <td>Pediatría</td>
+                  <td><input type="checkbox"></td>
+                </tr>
+                <tr>
+                  <td><a href="#" data-toggle="modal"  data-target="#verified_doc">Dra. Jane Eyre <span class="label label-success">Nuevo</span></a></td>
+                  <td><a href="#">Luis Joyanes</a></td>
+                  <td>27 Agosto 2014</td>
+                  <td>Ginecología</td>
+                  <td><input type="checkbox"></td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="row">
+            <div class="col-sm-8">
+              <button type="button" class="btn btn-default">Verificar</button>
+              <button type="button" class="btn btn-default">Descartar</button></div>
+            <div class="col-sm-4">
+             <ul class="pagination">
+              <li><a href="#">&laquo;</a></li>
+              <li class="active"><a href="#">1<span class="sr-only">(current)</span></a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#">4</a></li>
+              <li><a href="#">&raquo;</a></li>
+            </ul>
+             </div>
+            </div>
+            
+          </div>
+
+        </div>
       </div>
+
+
 <div class="modal fade" id="myModal">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -172,40 +259,97 @@
 </div><!-- /.modal -->
 
 
-<div class="modal fade" id="edit_doc">
+<div class="modal fade" id="verified_doc">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span>
           <span class="sr-only">Close</span></button>
-        <h4 class="modal-title">Modificar doctor o negocio</h4>
+        <h4 class="modal-title">Doctor Fulanito</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" role="form">
+ 
+  <div class="form-group">
+    <div class="col-sm-5">
+    <div class="fileinput fileinput-exists" data-provides="fileinput">
+    <div class="fileinput-exists thumbnail" style="width: 200px; height: 150px;">
+      <img src="../images/macias.jpeg">
+    </div>
+   
+    </div>
+    <p><b>Categoría:</b> Especialidades clínicas </p>
+    <p><b>Especialidad:</b> Especialidad 1 </p>
+  </div>
+  <div class="col-sm-7">
+    <p>Ut simul appellantur cum, ut reque legimus ocurreret ius. Nec ne aliquam scriptorem, mundi primis recteque id sed, pri laudem meliore te. Eam nihil officiis conceptam ei, pro ex eros velit invenire. Zril petentium an est, amet putant eum eu, usu iudico possim voluptatum ad. An sea vidisse alienum. Nam modo agam eius ea, duo paulo propriae ei. Te case illud his, duo invenire vituperata liberavisse ex. Vix vero persius ut. </p>
+  </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-3">
+      <p><a href="#"><span class="fa fa-facebook dash"></span>@blabla</a></p>
+      
+    </div>
+    <div class="col-sm-3">
+      <p><a href="#"><span class="fa fa-twitter dash"></span>@blabla</a></p>
+  </div>
+</div>
+  <div class="form-group">
+    <div class="col-sm-12">
+    <p><b>Dirección:</b> Ut simul appellantur cum, ut reque legimus ocurreret ius. 
+      Nec ne aliquam scriptorem, mundi primis recteque id sed #45677</p>
+    </div>
+  </div>
+    
+  <div class="form-group">
+    <div class="col-sm-12">
+      <iframe width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?client=safari&q=phoenix+az&oe=UTF-8&ie=UTF8&hq=&hnear=Phoenix,+Maricopa,+Arizona&gl=us&ll=33.448377,-112.074037&spn=0.040679,0.07699&z=14&output=embed"></iframe><br><small>
+      <a href="http://maps.google.com/maps?client=safari&q=phoenix+az&oe=UTF-8&ie=UTF8&hq=&hnear=Phoenix,+Maricopa,+Arizona&gl=us&ll=33.448377,-112.074037&spn=0.040679,0.07699&z=14&source=embed" style="color:#0000FF;text-align:left">Alargar Mapa</a></small>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-3">
+      <button type="button" class="btn btn-primary">Verificar</button>
+    </div>
+    <div class="col-sm-3">
+      <button type="button" class="btn btn-primary">Editar</button>
+    </div>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="editModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span>
+          <span class="sr-only">Close</span></button>
+        <h4 class="modal-title">Editar Usuario</h4>
       </div>
       <div class="modal-body">
         <form class="form-horizontal" role="form">
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control focus" id="inputEmail3" value="Nombre doctor dummy">
+      <input type="text" class="form-control focus" id="inputEmail3" value="Nombre usuario dummy">
     </div>
   </div>
   <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Dirección</label>
+    <label for="inputPassword3" class="col-sm-2 control-label">Correo Electrónico</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="inputPassword3" value="Vis ad timeam vivendo necessitatibus, viris nonumy abhorreant id vel #34566">
     </div>
   </div>
-  <div class="form-group">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-10">
-  <iframe width="350" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?client=safari&q=phoenix+az&oe=UTF-8&ie=UTF8&hq=&hnear=Phoenix,+Maricopa,+Arizona&gl=us&ll=33.448377,-112.074037&spn=0.040679,0.07699&z=14&output=embed"></iframe><br><small><a href="http://maps.google.com/maps?client=safari&q=phoenix+az&oe=UTF-8&ie=UTF8&hq=&hnear=Phoenix,+Maricopa,+Arizona&gl=us&ll=33.448377,-112.074037&spn=0.040679,0.07699&z=14&source=embed" style="color:#0000FF;text-align:left">Alargar Mapa</a></small>
-  </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Descripcion</label>
-    <div class="col-sm-10">
-      <textarea class="form-control" rows="3">Ut simul appellantur cum, ut reque legimus ocurreret ius. Nec ne aliquam scriptorem, mundi primis recteque id sed, pri laudem meliore te. Eam nihil officiis conceptam ei, pro ex eros velit invenire. Zril petentium an est, amet putant eum eu, usu iudico possim voluptatum ad. An sea vidisse alienum. Nam modo agam eius ea, duo paulo propriae ei. Te case illud his, duo invenire vituperata liberavisse ex. Vix vero persius ut. Te invenire instructior mea, vis inani primis facilisis at, minimum molestie complectitur sit ea. Idque corpora pertinacia usu cu, vel eu possim detraxit gloriatur.</textarea>
-    </div>
-  </div>
+
   <div class="form-group">
     <div class="col-sm-2"></div>
     <div class="col-sm-10">
@@ -221,6 +365,107 @@
   </div>
 </div>
   </div>
+
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Ciudad</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" value="Tijuana">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Acerca de</label>
+    <div class="col-sm-10">
+      <textarea class="form-control" rows="3">Ut simul appellantur cum, ut reque legimus ocurreret ius. Nec ne aliquam scriptorem, mundi primis recteque id sed, pri laudem meliore te. Eam nihil officiis conceptam ei, pro ex eros velit invenire. Zril petentium an est, amet putant eum eu, usu iudico possim voluptatum ad. An sea vidisse alienum. Nam modo agam eius ea, duo paulo propriae ei. Te case illud his, duo invenire vituperata liberavisse ex. Vix vero persius ut. Te invenire instructior mea, vis inani primis facilisis at, minimum molestie complectitur sit ea. Idque corpora pertinacia usu cu, vel eu possim detraxit gloriatur.</textarea>
+    </div>
+  </div>
+  
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Facebook</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" value="www.facebook.com">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Twitter</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" value="www.twitter.com">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-10">
+      <div class="dropdown">
+      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
+        Tipo de Usuario
+        <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Administrador</a></li>
+        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Dueño</a></li>
+        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Usuario</a></li>
+      </ul>
+    </div>
+  </div>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Guardar</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="edit_doc">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span>
+          <span class="sr-only">Close</span></button>
+        <h4 class="modal-title">Modificar doctor o negocio</h4>
+        </div>
+      <div class="modal-body">
+        <form class="form-horizontal" role="form">
+ 
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control focus" id="inputEmail3" value="Nombre doctor dummy">
+    </div>
+    </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Dirección</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" value="Vis ad timeam vivendo necessitatibus, viris nonumy abhorreant id vel #34566">
+    </div>
+    </div>
+    <div class="form-group">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-10">
+<iframe width="350" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?client=safari&q=phoenix+az&oe=UTF-8&ie=UTF8&hq=&hnear=Phoenix,+Maricopa,+Arizona&gl=us&ll=33.448377,-112.074037&spn=0.040679,0.07699&z=14&output=embed"></iframe><br><small>
+<a href="http://maps.google.com/maps?client=safari&q=phoenix+az&oe=UTF-8&ie=UTF8&hq=&hnear=Phoenix,+Maricopa,+Arizona&gl=us&ll=33.448377,-112.074037&spn=0.040679,0.07699&z=14&source=embed" style="color:#0000FF;text-align:left">Alargar Mapa</a></small>
+
+</div>
+
+</div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Descripcion</label>
+  <div class="col-sm-10">
+      <textarea class="form-control" rows="3">Ut simul appellantur cum, ut reque legimus ocurreret ius. Nec ne aliquam scriptorem, mundi primis recteque id sed, pri laudem meliore te. Eam nihil officiis conceptam ei, pro ex eros velit invenire. Zril petentium an est, amet putant eum eu, usu iudico possim voluptatum ad. An sea vidisse alienum. Nam modo agam eius ea, duo paulo propriae ei. Te case illud his, duo invenire vituperata liberavisse ex. Vix vero persius ut. Te invenire instructior mea, vis inani.
+    </textarea>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-10">
+  </div>
+</div>
+  </div>
+
   <div class="form-group">
     <div class="col-sm-2"></div>
     <div class="col-sm-4">
@@ -240,7 +485,7 @@
   <div class="col-sm-6">
     <div class="dropdown">
     <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-      Especialidad 2
+  Especialidad 2
       <span class="caret"></span>
     </button>
     <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
@@ -273,31 +518,8 @@
       <a href="#" class="icon-left"><i class="fa fa-search-plus"></i></a>
       <a href="#" class="icon-right"><i class="fa fa-times"></i></a>
     </div>
-  </div>
-  <div class="item-box" style="width: 100px; height: 100px; float:left; margin:15px;">
-    <div class="media-container">
-      <img src="../images/max.jpg">
-      <a href="#" class="icon-left"><i class="fa fa-search-plus"></i></a>
-      <a href="#" class="icon-right"><i class="fa fa-times"></i></a>
     </div>
-  </div>
-  <div class="item-box" style="width: 100px; height: 100px; float:left; margin:15px;">
-    <div class="media-container">
-      <img src="../images/melissa.jpg">
-      <a href="#" class="icon-left"><i class="fa fa-search-plus"></i></a>
-      <a href="#" class="icon-right"><i class="fa fa-times"></i></a>
-    </div>
-  </div>
-  <div class="item-box" style="width: 100px; height: 100px; float:left; margin:15px;">
-    <div class="media-container">
-      <img src="../images/hector.jpg">
-      <a href="#" class="icon-left"><i class="fa fa-search-plus"></i></a>
-      <a href="#" class="icon-right"><i class="fa fa-times"></i></a>
-    </div>
-  </div>
-  </div>
-    
-  <div class="form-group">
+    <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <div class="checkbox">
         <label>
@@ -305,14 +527,13 @@
         </label>
       </div>
     </div>
-  </div>
+    </div>
 </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Guardar</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
 
---
+++ admin/admin_doctor_profile.php
@@ -0,0 +1,27 @@
+<div class="row">
+<div class="col-sm-8">
+  <h2 class="sub-header">Doctor Fulanito</h2>
+  <div class="rating" style="float:left; margin-top:-55px; margin-left:200px;">
+    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+  </div>
+  </div>
+  <div class="col-sm-1">
+    <button type="button" class="btn btn-primary">Editar</button>
+  </div>
+  <div class="col-sm-1">
+    <button type="button" class="btn btn-primary">Eliminar</button>
+  </div>
+</div>
+<div class="col-sm-10">
+    <div class="fileinput fileinput-exists" data-provides="fileinput">
+    <div class="fileinput-exists thumbnail" style="width: 200px; height: 150px;">
+      <img src="../images/macias.jpeg">
+    </div>
+    <div>
+      <span class="btn btn-default btn-file"><span class="fileinput-new">Selecciona imagen principal</span><span class="fileinput-exists">Change</span><input type="file" name="..."></span>
+      <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+    </div>
+  </div>
+</div>
+
+
