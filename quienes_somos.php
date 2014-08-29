2f580365426a624e7ab4dd28a7a8b532bde73809
Israel Villalobos <israel-villalobos@hotmail.com>
2014-08-28 17:06:19 -0700

    nose que hice
    asdsad

---
+++ quienes_somos.php
@@ -0,0 +1,88 @@
+<div class="space60"></div>
+
+<div class="container">
+    <div class="row">
+      
+      <div class="col-md-8">  
+	<!--Acordion-->	
+		<div class="accordion" id="accordion2">
+          <div class="accordion-group">
+            <div class="accordion-heading">
+              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+                <h6>¿Quienes somos?</h6>
+              </a>
+            </div>
+            <div id="collapseOne" class="accordion-body collapse in">
+              <div class="accordion-inner" align="justify">
+              	<p>
+				Somos jóvenes con experiencia el sector de los servicios médicos, comprometidos con el usuario de los servicios y consientes de la necesidad de encontrar una solución rápida y confiable a los distintos problemas de salud que nuestra comunidad presenta. Como nuestro slogan lo dice  <strong>“Lo mas importante tu salud, nuestro compromiso la honestidad”.</strong>
+				</p>
+				<p>
+				Nuestra base de datos será alimentada por las aportaciones, comentarios y calificaciones que nuestros usuarios aporten, dando un valor agregado y sentido de pertenencia a la comunidad, respetando siempre las opiniones con total responsabilidad y legalidad.
+				</p>
+      <!--
+				<div align="right">
+				<strong>C.P. Y MBA Omar V. Tumalan Lemus</strong>
+				<br>
+				<strong>Ing. Y MBA Manuel A. Soto Madinaveitia</strong>
+              	</div>
+      -->
+              </div>
+            </div>
+          </div>
+
+          <A name="Mision"></A>
+          <div class="accordion-group">
+            <div class="accordion-heading">
+              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">   
+				<h6>Misión</h6>
+              </a>
+            </div>
+            <div id="collapseTwo" class="accordion-body collapse in">
+              <div class="accordion-inner" align="justify">
+				Ser el líder en la presentación de información confiable acerca de la oferta de servicios médicos que existen en la región, mediante las propuestas, revisiones y calificaciones de los miembros de la comunidad,  basadas en la  honestidad y la experiencia, comprometidos con los usuarios y los profesionales de la salud, con el firme propósito de ayudar a la comunidad a encontrar una optima solución a sus necesidades de salud.
+              </div>
+            </div>
+          </div>
+
+          <A name="Vision"></A>
+          <div class="accordion-group">
+            <div class="accordion-heading">
+              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+                <h6>Visión</h6>
+              </a>
+            </div>
+            <div id="collapseThree" class="accordion-body collapse in">
+              <div class="accordion-inner" align="justify">
+                Crear la mejor comunidad de usuarios de servicios médicos  con identidad propia, basada en los principios de honestidad, confianza y respeto hacia los miembros de la comunidad, buscando promover aquellos servicios médicos accesibles, seguros, eficientes y con la calidad y calidez  comprobada que el paciente se merece.
+              </div>
+            </div>
+          </div>
+        </div>
+    	<!--Acordion End-->	
+        <div class="space40"></div>  
+      </div>  
+      
+      <div class="col-md-4">
+        <!-- Progress Bar -->
+
+        <img src="images/quienes somos.png" alt="Smiley face" width="100%" height="100%">
+        <br>
+        <div class="client-says">
+          <div class="client-text">   
+            "La búsqueda de mejores opciones de servicios médicos y la oportunidad de adquirirlos fuera de los Estados Unidos, sin que el desplazamiento genere grandes costos para los habitantes de dicha región, han  propiciado la región de turismo medico con mayor potencial en el mundo.
+Los servicios médicos ofrecidos en el corredor turístico, desde Algodones en el municipio de Mexicali, hasta Ensenada, son de primera calidad y a precios mucho mas accesibles que en el estado de California."
+          </div>  
+          <div class="client-name">
+            <i class="fa fa-quote-right"></i><strong>Omar Tumalan</strong>, TjMed.
+          </div>  
+        </div>
+        <!-- Progress Bar End -->
+        <div class="space40"></div>  
+      </div>
+         
+    </div>
+  </div>
+
+		<div class="space60"></div>
+		<!-- Content End -->
