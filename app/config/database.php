03ff3cca49b47735ba338cb60e877702dbb0efbe
Israel Villalobos <israel-villalobos@hotmail.com>
2014-10-28 16:11:01 -0700

    Arregle unas cosillas que se veian feo, el footer, hay un nuevo width de articulos y de especialidades (8-4) y no me salió lo de cambiarle de color de alado :(

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'tjmed_v2',
+			'database'  => 'tjmed',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
